00dc013388bc165aaf186286321bf5b040756c5c
cristian gertui <ciakciak72@gmail.com>
2020-04-27 22:16:07 +0200

    Aggiungere i file di progetto.

---
+++ Fire_and_Forget/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LibraryTask;
+
+namespace Fire_and_Forget
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        CancellationTokenSource cts;
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            cts = new CancellationTokenSource();
+            Work wrk = new Work( 10, 1000, cts);
+            wrk.start();
+            MessageBox.Show("posso farti leggere questo messaggio mentre sto anche contando");
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            if (cts!=null)
+            {
+                cts.Cancel();
+            }
+        }
+    }
+}
---
+++ LibraryTask/Work.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LibraryTask
+{
+    public class Work
+    {
+        CancellationTokenSource _cts;
+        int _max;
+        int _ritardo;
+
+        //costruttore
+        public Work (int max, int ritardo, CancellationTokenSource cts)
+        {
+            _max = max;
+            _ritardo = ritardo;
+            _cts = cts;
+        }
+
+        public void start()
+        {
+            Task.Factory.StartNew(DoWork);
+        }
+
+        private void DoWork()
+        {
+            for (int i = 0; i < _max; i++)
+            {
+                Thread.Sleep(_ritardo);
+                if (_cts.IsCancellationRequested)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
